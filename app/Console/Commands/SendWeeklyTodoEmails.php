59ea0265676485fb3862a838afc6226b24853438
tasnimalshair <tasnimjalshair2002@gmail.com>
2025-07-09 09:07:58 +0300

    Schedule the notifications

---
+++ app/Console/Commands/SendWeeklyTodoEmails.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Todo;
+use App\Models\User;
+use App\Notifications\CompletedTodo;
+use Illuminate\Console\Command;
+
+class SendWeeklyTodoEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'emails:weekly-todos';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send weekly summary of completed todos to users';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            $todos = Todo::where('user_id' . $user->id)
+                ->where('is_completed', true)
+                // ->whereBetween('updated_at', [now()->startOfWeek(), now()->endOfWeek()])
+                ->get();
+
+            if ($todos->isNotEmpty()) {
+                $user->notify(new CompletedTodo($todos));
+            }
+        }
+
+        $this->info('Weekly todo emails sent!');
+    }
+}
